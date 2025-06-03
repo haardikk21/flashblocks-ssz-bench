88894f3b558c92c8bae308928096cb81d4f25cf4
Haardik H <haardik.haardik@coinbase.com>
2025-06-03 19:28:47 -0400

    hacky

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::Write, time::Duration};
+
+use flate2::{Compression, write::GzEncoder};
+use ssz::Encode;
+use tokio_tungstenite::tungstenite::http::Uri;
+
+use crate::{payload::FlashblocksPayloadV1, subscriber::WebsocketSubscriber};
+
+mod payload;
+mod subscriber;
+
+#[tokio::main]
+async fn main() {
+    let subscriber =
+        WebsocketSubscriber::new(Uri::from_static("wss://sepolia.flashblocks.base.org/ws"));
+    let flashblocks = subscriber
+        .gather_flashblocks(Duration::from_secs(10))
+        .await
+        .unwrap();
+
+    let _ = encode_as_json(&flashblocks);
+    let _ = encode_as_gzip_json(&flashblocks);
+    let _ = encode_as_ssz(&flashblocks);
+    let _ = encode_as_gzip_ssz(&flashblocks);
+}
+
+fn encode_as_json(flashblocks: &Vec<FlashblocksPayloadV1>) -> Vec<u8> {
+    let serialized = serde_json::to_vec(&flashblocks).unwrap();
+    println!("JSON bytes len: {:?}", serialized.len());
+    serialized
+}
+
+fn encode_as_gzip_json(flashblocks: &Vec<FlashblocksPayloadV1>) -> Vec<u8> {
+    let serialized = serde_json::to_vec(&flashblocks).unwrap();
+    let mut gz_encoder = GzEncoder::new(Vec::new(), Compression::default());
+    gz_encoder.write_all(&serialized).unwrap();
+    let compressed = gz_encoder.finish().unwrap();
+
+    println!("GZIP JSON bytes len: {:?}", compressed.len());
+
+    compressed
+}
+
+fn encode_as_ssz(flashblocks: &Vec<FlashblocksPayloadV1>) -> Vec<u8> {
+    let serialized = flashblocks.as_ssz_bytes();
+    println!("SSZ bytes len: {:?}", serialized.len());
+    serialized
+}
+
+fn encode_as_gzip_ssz(flashblocks: &Vec<FlashblocksPayloadV1>) -> Vec<u8> {
+    let serialized = flashblocks.as_ssz_bytes();
+    let mut gz_encoder = GzEncoder::new(Vec::new(), Compression::default());
+    gz_encoder.write_all(&serialized).unwrap();
+    let compressed = gz_encoder.finish().unwrap();
+
+    println!("GZIP SSZ bytes len: {:?}", compressed.len());
+    compressed
 }
---
+++ src/subscriber.rs
@@ -0,0 +1,57 @@
+use std::error::Error;
+use std::time::Duration;
+
+use futures_util::StreamExt;
+use tokio::{select, time::sleep};
+use tokio_tungstenite::{connect_async, tungstenite::http::Uri};
+
+use crate::payload::FlashblocksPayloadV1;
+
+pub struct WebsocketSubscriber {
+    uri: Uri,
+}
+
+impl WebsocketSubscriber {
+    pub fn new(uri: Uri) -> Self {
+        Self { uri }
+    }
+
+    pub async fn gather_flashblocks(
+        &self,
+        duration: Duration,
+    ) -> Result<Vec<FlashblocksPayloadV1>, Box<dyn Error>> {
+        println!("Gathering flashblocks for {} seconds", duration.as_secs());
+
+        let mut flashblocks = Vec::new();
+        let (ws_stream, _) = connect_async(&self.uri).await.unwrap();
+        let (_, mut read) = ws_stream.split();
+
+        let sleep = sleep(duration);
+        tokio::pin!(sleep);
+
+        loop {
+            select! {
+                () = &mut sleep => {
+                    break;
+                }
+
+                Some(message) = read.next() => {
+                    match message {
+                        Ok(msg) => {
+                            let text = msg.to_text()?;
+                            let flashblock = serde_json::from_str::<FlashblocksPayloadV1>(&text).unwrap();
+                            flashblocks.push(flashblock);
+                        }
+                        Err(e) => {
+                            return Err(Box::new(e));
+                        }
+                    }
+                }
+            }
+        }
+
+        println!("Received {} flashblocks", flashblocks.len());
+
+        Ok(flashblocks)
+    }
+}
